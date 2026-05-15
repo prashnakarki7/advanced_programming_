b51421e07c4c06e0fb2df779dcb1b9b10934b6fb
Luja Kauri <luzakauri230@gmail.com>
2026-05-15 12:08:33 +0545

    Commit of ComedyServlet, HorrorServlet, NepaliLiteratureServlet and Romance Servlet

---
+++ src/main/java/com/advance_programming/controller/HorrorServlet.java
@@ -0,0 +1,24 @@
+package com.advance_programming.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/horror")
+public class HorrorServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        // Forwarding to the horror JSP
+        request.getRequestDispatcher("/WEB-INF/pages/horror.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
